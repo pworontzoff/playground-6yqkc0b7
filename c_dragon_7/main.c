101bb76c859b90ce34254e53fd89e6450e87e16c
pworontzoff <pierre.worontzoff@hepl.be>
2024-03-21 12:35:32 +0100

    Update main.c

--- c_dragon_7/main.c
+++ c_dragon_7/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "math.h"
 #include "drawing.h"
 
 
 #define COURBE_NIVEAU 4
 
 
-#define LARGEUR 1200
-#define HAUTEUR 1200
+#define LARGEUR 740
+#define HAUTEUR 510
 #define TAILLE_SEGMENT_INIT 400
 
 
@@ -15,7 +16,6 @@ int chemin[65536]={0};
 int etape=0;
 int longueur_segment = TAILLE_SEGMENT_INIT;
 
-int nbVirages();
 void ajoutePlis();
 void init_dragon();
 
@@ -23,44 +23,23 @@ int main() {
     int i=0;
     
     init_dragon();
-	
+    init_drawing(LARGEUR,HAUTEUR);
+
     repeat (10) {
-		draw(50);
+		draw(25);
 		turn(90,RIGHT);
-		draw(50);
+		draw(25);
 		turn(90,LEFT);
     } loop;
+
     
     display_drawing();
 
     return 0;
 }
 
-int nbVirages() {
-    /*
-    etape  chemin                       - nombre de virages
-           debut    milieu fin            debut milieu fin
-    1               G                   - 0     1      0   =   1 + 2 x 0  =  1  virage
-    2      G        G      D            - 1     1      1   =   1 + 2 x 1  =  3  virages
-    3      GGD      G      GDD          - 3     1      3   =   1 + 2 x 3  =  7  virages
-    4      GGDGGDD  G      GGDDGDD      - 7     1      7   =   1 + 2 x 7  =  15 virages
-    ...
-    x-1                                                                   =  p  virages
-    x                                                      =   1 + 2 x p        virages
-    ...
-    */
-
-    int p=0;
-
-    repeat (etape) {
-        p = 1 + 2 * p;
-    } loop;
-
-    return p;
-}
-
 void ajoutePlis() {
-    int nouveauMilieu = nbVirages();
+    int nouveauMilieu = pow(2,etape)-1;
     int i=1;
 
     G(chemin, nouveauMilieu);
@@ -84,6 +63,5 @@ void init_dragon() {
         longueur_segment = (longueur_segment * (7 + (0.027 * COURBE_NIVEAU))) / 10;        
     } loop;
 
-    init_drawing(LARGEUR,HAUTEUR);
     return;
 }
